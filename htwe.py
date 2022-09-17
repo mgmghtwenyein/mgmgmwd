01f59d77deba254ea26fcb177ae95a733cf0bb9f
mgmghtwenyein <113499671+mgmghtwenyein@users.noreply.github.com>
2022-09-17 23:16:12 +0630

    htwe.py

---
+++ htwe.py
@@ -0,0 +1,5 @@
+﻿. /data/data/com.termux/files/usr/etc/profile
+command_not_found_handler() {
+        /data/data/com.termux/files/usr/libexec/termux/command-not-found $1
+}
+figlet -f standard Mg Mg MWD
