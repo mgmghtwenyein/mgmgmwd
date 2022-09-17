342e0b83c34560adf85191035bb719bcef90c0f1
mgmghtwenyein <113499671+mgmghtwenyein@users.noreply.github.com>
2022-09-17 23:10:13 +0630

    Add files via upload

---
+++ mgmgmwd12.sh
@@ -0,0 +1,7 @@
+﻿#!/data/data/com.termux/files/usr/bin/bash
+echo "alias chfont='$HOME/.termux/fonts.sh'" >> "$HOME/.mgmg"
+echo "alias chcolor='$HOME/.termux/colors.sh'" >> "$HOME/.mgmg"
+
+echo "Please restart termux!"
+
+exit
